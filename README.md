e24a3e6451d24a420a9f149639a644db5218ef7a
bagasri <bagasri212@gmail.com>
2020-11-18 22:40:50 +0700

    Update

---
+++ README.md
@@ -0,0 +1,15 @@
+# FindJobs
+
+FindJobs merupakan aplikasi yang akan membantu anda menemukan
+pekerjaan yang cocok, sesuai bakat dan minat anda. Proses
+mudah dan lebih singkat.
+
+## Nama Kelompok
+
+- Bagas Ramadhani Issanto (182410103050)
+- Dita Shahihah (182410103044)
+- Pradana Rizki Susanto (182410103020)
+
+### Progress
+
+- ☑ Membuat StartPage
---
+++ lib/Start.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import 'package:cobaaa/widgets/original_button.dart';
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SizedBox(),
+              Image.asset('assets/images/logo.png'),
+              OriginalButton(
+                text: 'Get Started',
+                onPressed: () => Navigator.of(context).pushNamed('login'),
+                textColor: Colors.lightBlue,
+                bgColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
