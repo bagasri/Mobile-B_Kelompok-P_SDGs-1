9ada64612b453dfb9644dd1c49320086fa008c87
bagasri <bagasri212@gmail.com>
2021-01-09 13:16:20 +0700

    Delete

--- lib/resources/gatway/api_gateway.dart
+++
@@ -1,6 +0,0 @@
-
-import 'package:findjobs/models/job/job_model.dart';
-
-abstract class ApiGateway {
-  Future<List<JobModel>> getJobs({String description, String location, bool isFullTime,int page});
-}
